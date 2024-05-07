86df40c74e787d2cb2e6e6d8784361a40971ab6c
nouman qureshi <noumanqureshi15@gmail.com>
2024-05-08 03:58:40 +0500

    add daily income

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -32,6 +32,8 @@ const Dashboard = () => {
   const [userInfo, setuserInfo] = useState({})
   const [referralMember, setreferralMember] = useState({})
   const [earnings, setEarnings] = useState({})
+  const [Dailycomerecord, setDailycomerecord] = useState([])
+
   const baseUrl = 'https://starfish-app-7c9pu.ondigitalocean.app'
   // const { id } = router.query
   // const navItems = navigation(id)
@@ -59,6 +61,20 @@ const Dashboard = () => {
         // Note: Axios automatically converts objects to JSON, so no need to stringify
       })
 
+      // Axios wraps the response data inside a `data` property
+      return response.data // Assuming the response contains the user info
+    } catch (error) {
+      throw new Error('Token validation failed with status: ' + error.response.status)
+    }
+  }
+  const getdailyincome = async (token, userId) => {
+    try {
+      const response = await axios.get(`${baseUrl}/dailyincome/${userId}`, {
+        headers: {
+          Authorization: `Bearer ${token}` // Using Bearer token for authentication
+        }
+        // Note: Axios automatically converts objects to JSON, so no need to stringify
+      })
       // Axios wraps the response data inside a `data` property
       return response.data // Assuming the response contains the user info
     } catch (error) {
@@ -95,6 +111,8 @@ const Dashboard = () => {
         setreferralMember(referralinfo.user)
         setuserInfo(userInfo.user)
         const earning = await getearninginfo(token, userInfo.user?._id)
+        const dailyincomedata = await getdailyincome(token, userInfo.user?._id)
+        setDailycomerecord(dailyincomedata.dailyincomeData?.dailyIncome)
         setEarnings(earning.earnings)
         // Here, you can set user info in global state/context
       } catch (error) {
@@ -111,7 +129,53 @@ const Dashboard = () => {
           <Trophy userInfo={userInfo} earnings={earnings} />
         </Grid>
         <Grid item xs={12} md={8}>
-          <StatisticsCard />
+          {/* <StatisticsCard /> */}
+        </Grid>
+        <Grid item xs={12} md={6} lg={4}>
+          <Grid container spacing={6}>
+            <Grid item xs={6}>
+              <CardStatisticsVerticalComponent
+                stats={'Rs ' + Dailycomerecord[0]?.amount}
+                icon={<Poll />}
+                color='success'
+                // trendNumber='+42%'
+                title='Today Earning'
+                subtitle='Profit'
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <CardStatisticsVerticalComponent
+                stats='0'
+                title='Last 7 Days Earning'
+                trend='negative'
+                color='secondary'
+                trendNumber='-15%'
+                subtitle='Past Week'
+                icon={<CurrencyUsd />}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <CardStatisticsVerticalComponent
+                stats='0'
+                trend='negative'
+                trendNumber='-18%'
+                title='Last 30 Days Earning'
+                subtitle='Past Month'
+                icon={<BriefcaseVariantOutline />}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <CardStatisticsVerticalComponent
+                stats='0'
+                color='warning'
+                trend='negative'
+                trendNumber='-18%'
+                subtitle='Overall'
+                title='All Times Earning'
+                icon={<HelpCircleOutline />}
+              />
+            </Grid>
+          </Grid>
         </Grid>
         <Grid item xs={12} md={6} lg={4}>
           <WeeklyOverview />
@@ -119,52 +183,7 @@ const Dashboard = () => {
         <Grid item xs={12} md={6} lg={4}>
           {/* <TotalEarning /> */}
         </Grid>
-        {/* <Grid item xs={12} md={6} lg={4}>
-          <Grid container spacing={6}>
-            <Grid item xs={6}>
-              <CardStatisticsVerticalComponent
-                stats='$25.6k'
-                icon={<Poll />}
-                color='success'
-                trendNumber='+42%'
-                title='Total Profit'
-                subtitle='Weekly Profit'
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <CardStatisticsVerticalComponent
-                stats='$78'
-                title='Refunds'
-                trend='negative'
-                color='secondary'
-                trendNumber='-15%'
-                subtitle='Past Month'
-                icon={<CurrencyUsd />}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <CardStatisticsVerticalComponent
-                stats='862'
-                trend='negative'
-                trendNumber='-18%'
-                title='New Project'
-                subtitle='Yearly Project'
-                icon={<BriefcaseVariantOutline />}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <CardStatisticsVerticalComponent
-                stats='15'
-                color='warning'
-                trend='negative'
-                trendNumber='-18%'
-                subtitle='Last Week'
-                title='Sales Queries'
-                icon={<HelpCircleOutline />}
-              />
-            </Grid>
-          </Grid>
-        </Grid> */}
+
         <Grid item xs={12} md={6} lg={4}>
           {/* <SalesByCountries /> */}
         </Grid>
